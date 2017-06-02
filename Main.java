37ed1f74b4bd5c4e7f3b7ac8c9eeacd0c4f6c0e7
muni1209 <munig1209@gmail.com>
2017-06-02 15:26:47 +0530

    Update Main.java
    remote repository to local repository

--- Main.java
+++ Main.java
@@ -3,6 +3,8 @@ class Main
 	public static void main(String[] args) 
 	{
 		System.out.println("add the new lines to Git");
+		System.out.println("add some line to remote repository to local repository");
+		System.out.println("add the local machine value");
 		System.out.println("Hello World!");
 	}
 }
